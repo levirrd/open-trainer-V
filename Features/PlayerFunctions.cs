6307ac4871bc0f5c30386bd30a3d2963586c4767
levirrd <demsa.levente@outlook.com>
2025-06-15 15:19:35 +0200

    Did some tweaks for ToggleGodmode -healplayer -infinitestamina -cleanped -setplayervisible -setplayercash -setplayerwantedlevel -neverwanted -changed font of banner + some new items and stuff --- todo change the wanted level slider

--- Features/PlayerFunctions.cs
+++ Features/PlayerFunctions.cs
@@ -1,4 +1,5 @@
-﻿using GTA; 
+﻿using GTA;
+using GTA.Native;
 
 namespace Open_Trainer_V.Features
 {
@@ -13,11 +14,19 @@ namespace Open_Trainer_V.Features
         }
         public static void ToggleGodMode(bool isEnabled)
         {
-            if (isEnabled) playerChar.IsInvincible = true;
-            else playerChar.IsInvincible = false;
-            GTA.UI.Notification.Show("~b~God Mode: " + (isEnabled ? "~g~Enabled" : "~r~Disabled"));
+            playerChar.IsInvincible = isEnabled;
+            playerChar.CanRagdoll = !isEnabled;
+            playerChar.IsBulletProof = isEnabled;
+            playerChar.IsCollisionProof = isEnabled;
+            playerChar.IsExplosionProof  = isEnabled;
+            playerChar.IsFireProof  = isEnabled;
+            playerChar.IsMeleeProof = isEnabled;
+            playerChar.IsSmokeProof = isEnabled;
+            playerChar.IsSteamProof = isEnabled;
+            playerChar.IsWaterCannonProof  = isEnabled;
+            string statusText = isEnabled ? "~g~Enabled" : "~r~Disabled";
+            GTA.UI.Notification.Show($"~b~God Mode: {statusText}");
         }
-
         public static void HealPlayer()
         {
             int maxHealth = playerChar.MaxHealth;
@@ -25,6 +34,64 @@ namespace Open_Trainer_V.Features
             playerChar.Health = maxHealth;
             playerChar.Armor = maxArmor;
             GTA.UI.Notification.Show("Player has been healed.");
+        }
+        public static bool isInfiniteStaminaOn;
+        public static void InfiniteStamina(bool isEnabled)
+        {
+            Function.Call(Hash.RESET_PLAYER_STAMINA, Game.Player);
+ 
+            isInfiniteStaminaOn = isEnabled;
+            string statusText = isEnabled ? "~g~Enabled" : "~r~Disabled";
+            GTA.UI.Notification.Show($"~b~Infinite stamina: {statusText}");
+        }
+        public static void CleanPed()
+        {
+            playerChar.ClearBloodDamage();
+            playerChar.ClearVisibleDamage();
+            GTA.UI.Notification.Show("~b~Cleaned Ped.");
+        }
+
+        public static void SetPlayerVisible(bool IsVisible)
+        {
+            playerChar.IsVisible = !IsVisible;
+            string statusText = IsVisible ? "~g~Invisible" : "~r~Visible";
+            GTA.UI.Notification.Show($"~b~Player visibility: {statusText}");
+        }
+
+        public static void SetPlayerCash()
+        {
+            string result = Game.GetUserInput();
+            if (int.TryParse(result, out int value))
+            {
+                string statName;
+                if (playerChar.Model.Hash == Game.GenerateHash("player_zero"))
+                    statName = "SP0_TOTAL_CASH"; // Michael
+                else if (playerChar.Model.Hash == Game.GenerateHash("player_one"))
+                    statName = "SP1_TOTAL_CASH"; // Franklin
+                else if (playerChar.Model.Hash == Game.GenerateHash("player_two"))
+                    statName = "SP2_TOTAL_CASH"; // Trevor
+                else
+                {
+                    GTA.UI.Notification.Show("~r~Unknown character model. Cannot set cash.");
+                    return;
+                }
+                Function.Call(Hash.STAT_SET_INT, Game.GenerateHash(statName), value, true);
+                GTA.UI.Notification.Show($"~g~Set money to: ~s~${value}");
+            }
+            else
+            {
+                GTA.UI.Notification.Show("~r~Invalid number entered.");
+            }
+        }
+
+        public static void SetPlayerWantedLevel(int wantedLevel)
+        {
+            Game.Player.WantedLevel = wantedLevel;
+        }
+
+        public static void NeverWanted()
+        {
+            Game.MaxWantedLevel = 0;
         }
     }
 }
--- MenuScript/MenuScript.cs
+++ MenuScript/MenuScript.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.Remoting.Channels;
+using GTA.UI;
 using LemonUI;
 using LemonUI.Menus;
 using Open_Trainer_V.Features; 
@@ -20,7 +21,14 @@ namespace Open_Trainer_V
         //player
         private readonly NativeItem ClearWanted;
         private readonly NativeCheckboxItem GodMode;
+        private readonly NativeCheckboxItem InfiniteStamina;
         private readonly NativeItem HealPlayer;
+        private readonly NativeItem CleanPlayer;
+        private readonly NativeCheckboxItem PlayerVisibility;
+        private readonly NativeItem SetMoney;
+        private readonly NativeSliderItem SetWantedLevel;
+        private readonly NativeCheckboxItem NeverWanted;
+        
         private readonly NativeItem FixVehicle;
         public MenuScript()
         {
@@ -43,31 +51,69 @@ namespace Open_Trainer_V
             TrainerMenu.AddSubMenu(WeaponOptions);
             TrainerMenu.AddSubMenu(VehicleOptions);
             TrainerMenu.AddSubMenu(WorldOptions);
-            
+            TrainerMenu.BannerText.Font = Font.ChaletComprimeCologne;
+            TrainerMenu.MouseBehavior = MenuMouseBehavior.Movement;
             FixVehicle = new NativeItem("Fix Vehicle", "Repairs and cleans the vehicle");
             ClearWanted = new NativeItem("Clear Wanted", "Clears Wanted Level");
             GodMode = new NativeCheckboxItem("God Mode", "Sets God Mode",false);
             HealPlayer = new NativeItem("Heal Player", "Heals the player (armor+health)");
+            InfiniteStamina = new NativeCheckboxItem("Infinite Stamina", "Infinite Stamina Level", false);
+            CleanPlayer = new NativeItem("Clean Player", "Cleans the player");
+            PlayerVisibility = new NativeCheckboxItem("Player Visibility", "Sets player visibility",false);
+            SetMoney = new NativeItem("Money Input", "Money Input");
+            SetWantedLevel = new NativeSliderItem("Set Wanted Level", "Sets Wanted Level", 5,0);
+            NeverWanted = new  NativeCheckboxItem("Never Wanted", "Never Wanted",false);
+            
             //add items to menus or submenus
             PlayerOptions.Add(ClearWanted);
             PlayerOptions.Add(GodMode);
+            PlayerOptions.Add(InfiniteStamina);
             PlayerOptions.Add(HealPlayer);
+            PlayerOptions.Add(CleanPlayer);
+            PlayerOptions.Add(PlayerVisibility);
+            PlayerOptions.Add(SetWantedLevel);
+            PlayerOptions.Add(SetMoney);
+            PlayerOptions.Add(NeverWanted);
+            
             VehicleOptions.Add(FixVehicle);
+            
             //funcs
-            GodMode.CheckboxChanged += (sender, e) => 
+            GodMode.CheckboxChanged += (sender, e) =>
             {
-               if(GodMode.Checked) PlayerFunctions.ToggleGodMode(true);
-               else PlayerFunctions.ToggleGodMode(false);
+                PlayerFunctions.ToggleGodMode(GodMode.Checked);
             };
-
+            InfiniteStamina.CheckboxChanged += (sender, e) =>
+            {
+                PlayerFunctions.InfiniteStamina(InfiniteStamina.Checked);
+            };
             HealPlayer.Activated += (sender, e) => PlayerFunctions.HealPlayer();
             ClearWanted.Activated += (sender, args) =>PlayerFunctions.ClearWantedLevel();
-            
+            CleanPlayer.Activated += (sender, args) => PlayerFunctions.CleanPed();
+            PlayerVisibility.CheckboxChanged += (sender, args) =>
+            {
+                PlayerFunctions.SetPlayerVisible(PlayerVisibility.Checked);
+            };
+            SetMoney.Activated += (sender, args) =>
+            {
+                PlayerFunctions.SetPlayerCash();
+            };
+            SetWantedLevel.ValueChanged += (sender, args) =>
+            {
+                PlayerFunctions.SetPlayerWantedLevel(SetWantedLevel.Value);
+            };
+            NeverWanted.CheckboxChanged += (sender, args) =>
+            {
+                PlayerFunctions.NeverWanted();
+            };
         }
         
         public void Tick()
         {
             menuPool.Process();
+            if (PlayerFunctions.isInfiniteStaminaOn && InfiniteStamina.Checked)
+            {
+                PlayerFunctions.InfiniteStamina(InfiniteStamina.Checked);
+            } 
         }
 
         public void OpenMenu()
